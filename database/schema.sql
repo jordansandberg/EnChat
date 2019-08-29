82fbf8d9a40d6f8787c79b1fdc1c44cb553f9205
jordan <jordansandberg1@gmail.com>
2019-08-29 12:29:17 -0700

    EC-3: Create Database
    create schema for database

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,4 @@
-drop table if exists chat_room;
+drop table if exists chat_room cascade;
 
 create table chat_room(
     chat_room_id serial primary key,
@@ -6,23 +6,25 @@ create table chat_room(
     chat_room_name varchar(32) not null
 );
 
-drop table if exists user;
+drop table if exists users cascade;
 
-create table user(
-    serial usr_id primary key,
+create table users(
+    usr_id serial primary key,
     pword varchar(32) not null, 
     usr_name varchar(32) not null
 );
 
-drop table if exists whitelist(
+drop table if exists whitelist cascade;
+
+create table whitelist(
     whitelist_id serial primary key
 );
 
-drop table if exists ownse ;
+drop table if exists owns;
 
 create table owns(
     chat_room_id serial references chat_room(chat_room_id),
-    usr_ir serial references user(usr_id),
+    usr_id serial references users(usr_id),
     primary key (chat_room_id, usr_id)
 );
 
@@ -30,22 +32,25 @@ drop table if exists sends_message;
 
 create table sends_message(
     chat_room_id serial references chat_room(chat_room_id),
-    usr_id serial references user(usr_id),
+    usr_id serial references users(usr_id),
     txt varchar(250) not null,
-    time_stamp datetime
-)
+    time_stamp date,
+    primary key (chat_room_id, usr_id)
+);
 
 drop table if exists has_a;
 
 create table has_a(
     whitelist_id serial references whitelist(whitelist_id),
-    chat_room_id serial references chat_room(chat_room_id)
+    chat_room_id serial references chat_room(chat_room_id),
+    primary key (whitelist_id, chat_room_id)
 );
 
 drop table if exists is_on;
 
 create table is_on(
-    whitelist_id references whitelist(whitelist_id),
-    usr_id references user(usr_id)
+    whitelist_id serial references whitelist(whitelist_id),
+    usr_id serial references users(usr_id),
+    primary key (whitelist_id, usr_id)
 );
 
